97192ca95de6c42c3b74c547254c64df1fcd7fe3
Desouky <alaa.desouky33@gmail.com>
2021-06-27 00:27:13 +0200

    trillo app initial setup

---
+++ pages/index.js
@@ -0,0 +1,12 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <link rel="shortcut icon" type="image/png" href="/favicon.png" />
+        <title>Trillo &mdash; Your all-in-one booking app</title>
+      </Head>
+    </>
+  )
+}
